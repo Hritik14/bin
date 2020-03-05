b50b7fe4da82e2d7392a568d635151f2f4a44d47
Hritik Vijay <hr1t1k@pm.me>
2020-03-06 00:07:15 +0530

    Uncluttered

--- i3/toggle_ffmpeg_screencast.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-if ! flock -Fn /tmp/ffmpeg_screencast.lock -c "$(dirname $0)/ffmpeg_screencast.sh"; then
-	rm /tmp/ffmpeg_screencast.filename
-	pkill -u "$USER" -f "ffmpeg.*x11grab"
-fi
